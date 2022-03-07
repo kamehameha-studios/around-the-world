d4eda69236839b42a5fa450f2cc52f8b369b1f59
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2022-03-07 14:25:59 -0600

    !

---
+++ index.js
@@ -0,0 +1,5 @@
+function WriteCookie(name, content, domain, path, sent_for,expires) {
+    cookievalue = this.cookievalue;
+    document.cookie = name = this.name; content = this.content; domain = this.domain; path = this.path; sent_for = this.sent_for; expires = this.expires;;
+};
+WriteCookie('a', 'none', 'kamehameha-studios.github.io', '/', '', '');
